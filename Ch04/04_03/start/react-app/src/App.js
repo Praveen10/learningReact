ed57de7c2800d5d3607c1f21434d7275c55de6f7
Praveen10 <praveenrajamani58@gmail.com>
2023-12-20 15:05:46 +0000

    Working on React

---
+++ Ch04/04_03/start/react-app/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useReducer } from "react";
+
+function App( { library } ) {
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
+
+  return (
+    <div className="App">
+      <input 
+        type="checkbox" 
+        value={checked} 
+        onChange={setChecked}>
+        </input>
+        <label>
+          {checked ? "checked" : "not checked"}
+        </label>
+    </div>
+  );
+}
+
+export default App;
