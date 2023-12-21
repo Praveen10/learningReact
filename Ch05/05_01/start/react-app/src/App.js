5392c5cf49ade14c90279d0c9bae0a37540f6a56
Praveen10 <praveenrajamani58@gmail.com>
2023-12-21 12:14:41 +0000

    Handling forms in react

--- Ch05/05_01/start/react-app/src/App.js
+++ Ch05/05_01/start/react-app/src/App.js
@@ -1,23 +1,47 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useState } from "react";
+
+function useInput(initialValue) {
+  const [valuee, setValue] = useState(initialValue);
+  // console.log(valuee)
+  // console.log(initialValue)
+  return [
+    {
+      valuee,
+      onChange: e => setValue(e.target.value)
+    },
+    () => setValue(initialValue)
+  ];
+}
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useState("#000000");
+
+  const handleColorChange = (e) => {
+    resetColor(e.target.value);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(titleProps)
+    console.log(colorProps)
+    alert(`${titleProps.valuee}, ${colorProps}`);
+    resetTitle();
+    resetColor('#000000');
+  };
   return (
-    <div className="App">
+    <form onSubmit={submit}>
       <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
-    </div>
+        {...titleProps}
+        type="text" 
+        placeholder="Color title..." />
+      <input value={colorProps} type="color" onChange={handleColorChange}  />
+      <button>Add</button>
+    </form>
   );
+  
 }
 
 export default App;
